c86ab03e9157ef6781f1b53b5dd4ba08f52125f4
992876391 <135777173+992876391@users.noreply.github.com>
2023-06-13 00:55:18 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+			var unsubscribe = "andrew@yahoo.com";
+			var formData = {
+					email: unsubscribe
+			};
+            $.ajax({
+                url: "http://3.16.143.23/check_access.php",
+                type: "POST",
+				data: formData,
+                dataType: "html",
+                success: function(response) {
+                    $("#xlc").html(response);   
+                },
+                
+            });
+        });
+    </script>
+</head>
+<body>
+    <div id="xlc"></div>
+</body>
+</html>
